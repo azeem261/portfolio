afc3f48443ad79ebdd3e1c5c8cfb80f32b60f9a2
azeem261 <azeem261@gmail.com>
2019-04-18 16:19:23 +0500

    BLOG and PROJECT Models

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=150)
+    post = models.TextField()
