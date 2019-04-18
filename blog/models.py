39995802043ff3f199333c252d53dfe0a7b21517
azeem261 <azeem261@gmail.com>
2019-04-18 17:06:27 +0500

    New

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,11 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
     pub_date = models.DateTimeField()
     title = models.CharField(max_length=150)
+    title = models.CharField(max_length=250)
     post = models.TextField()
+    # GROUPS = (
+    #     ('Sports', 'Sports'),
+    #     ('Exclusive', 'Exclusive'),
+    #     ('Entertainment', 'Entertainment'),
+    #     )
+    # category = models.CharField(max_length=20,choices=GROUPS, unique=True)
